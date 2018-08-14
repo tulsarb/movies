f6619eb2a50fb0b4348237fd456bb29c7dd68675
Josh Hamilton <nearbycoder@gmail.com>
2018-08-13 21:27:34 -0500

    fix endpoint to return empty array instead of erroring out (#10)

--- app/graphql/types/movie_pagination_type.rb
+++ app/graphql/types/movie_pagination_type.rb
@@ -3,7 +3,11 @@
 module Types
   class MoviePaginationType < Types::BaseObject
     field :movies, [MovieType], null: false
-    field :page, Int, null: false
-    field :total_pages, Int, null: false
+    field :page, Int, null: true
+    field :total_pages, Int, null: true
+
+    def movies
+      object.movies || []
+    end
   end
 end
